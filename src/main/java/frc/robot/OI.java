2491e812b42c1f5754d654e1459c521aefce7e61
trojanators <frc5740@gmail.com>
2018-12-17 12:49:27 -0800

    add project code

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.OI;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  public static Joystick controller = new Joystick(RobotMap.controllerPort);
+
+  public static WPI_VictorSPX leftFrontDriveMotor = new WPI_VictorSPX(RobotMap.leftFrontDrivePWM);
+  public static WPI_VictorSPX leftCenterDriveMotor = new WPI_VictorSPX(RobotMap.leftCenterDrivePWM);
+  public static WPI_VictorSPX leftBackDriveMotor = new WPI_VictorSPX(RobotMap.leftBackDrivePWM);
+  public static WPI_VictorSPX rightFrontDriveMotor = new WPI_VictorSPX(RobotMap.rightFrontDrivePWM);
+  public static WPI_VictorSPX rightCenterDriveMotor = new WPI_VictorSPX(RobotMap.rightCenterDrivePWM);
+  public static Spark rightBackDriveMotor = new Spark(RobotMap.rightBackDrivePWM);
+
+  public static SpeedControllerGroup leftDriveGroup = new SpeedControllerGroup(leftFrontDriveMotor, leftCenterDriveMotor, leftBackDriveMotor);
+  public static SpeedControllerGroup rightDriveGroup = new SpeedControllerGroup(rightFrontDriveMotor, rightCenterDriveMotor, rightBackDriveMotor);
+  public static DifferentialDrive drive = new DifferentialDrive(leftDriveGroup, rightDriveGroup);
+
+  public static Relay compressorSpike = new Relay(RobotMap.compressorSpikePort);
+  public static DigitalInput pressureSwitch = new DigitalInput(RobotMap.pressureSwitchPort);
+  public static DoubleSolenoid shiftSolenoid = new DoubleSolenoid(RobotMap.shiftSolenoidOne, RobotMap.shiftSolenoidTwo);
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class RobotMap {
+  public static int controllerPort = 0;
+
+  public static int leftFrontDrivePWM = 0;
+  public static int leftCenterDrivePWM = 1;
+  public static int leftBackDrivePWM = 2;
+  public static int rightFrontDrivePWM = 3;
+  public static int rightCenterDrivePWM = 4;
+  public static int rightBackDrivePWM = 5;
+
+  public static int compressorSpikePort = 0;
+  public static int pressureSwitchPort = 0;
+  public static int shiftSolenoidOne = 0;
+  public static int shiftSolenoidTwo = 1;
+}
